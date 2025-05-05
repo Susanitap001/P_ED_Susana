33b3df0fed1fc8e1ce36a12fdb86ce1c7b8a3029
Susana <susanaparralopitel@gmail.com>
2025-05-05 15:24:24 +0200

    Añadido método restar

--- src/javaapplication9/Calculadora.java
+++ src/javaapplication9/Calculadora.java
@@ -18,4 +18,7 @@ public class Calculadora {
         Calculadora calc = new Calculadora();
         System.out.println("2 + 2 = " + calc.sumar(2, 2));
     }
+        public double restar(double a, double b) {
+        return a - b;
+    }
 }
